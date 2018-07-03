eaa3c8bcd70c73ee67189ecf882c54870aecf5d9
jorgegordiano <40776840+jorgegordiano@users.noreply.github.com>
2018-07-02 22:08:07 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,17 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
-}
+	var nombre;
+	//nombre = "Jorge";
+	//alert(nombre);
+	nombre = prompt("ingrese su nombre", "natalia natalia");
+	alert("su nombre es:"+nombre);
 
+
+	//*var person = prompt("Please enter your name", "Harry Potter");
+	
+//comentario de una linea
+
+
+
+}
